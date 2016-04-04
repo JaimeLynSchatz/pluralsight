e438f9820ad2cdb8a5487a1e36b09300beaf88f4
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-04-04 07:02:46 -0700

    scooping up last changes from loop module

--- loops.rb
+++ loops.rb
@@ -5,3 +5,6 @@ text = while message = "here's a message"
   break "there's nothing there" if message == ''
 end
 
+# check out the redo loop, too
+# could be useful when getting input from the user
+
